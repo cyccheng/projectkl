e0e23e043a44d6c60d8d04d8f05442fef80c016c
KuetTai <kt.xtrik@gmail.com>
2014-07-23 09:48:35 +0800

    kt: main_page

--- app/routes.php
+++ app/routes.php
@@ -15,6 +15,10 @@ Route::get('/fd', function(){
 	return View::make('fdtest');
 });
 
+Route::get('/main', function(){
+	return View::make('main');
+});
+
 Route::get('/', function()
 {
 	echo 'hello';
